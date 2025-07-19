e870d3c01322c9bf4a8f7aca0c5a5981059f61e4
ROTEMIL95 <56782793+ROTEMIL95@users.noreply.github.com>
2025-07-20 00:42:58 +0300

    Update main.py to use environment variable for port configuration
    - Modified the main.py file to retrieve the port number from the environment variable "PORT", defaulting to 5000 if not set.
    - Improved flexibility for running the application in different environments.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 import uvicorn
 from app import app
+import os
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     uvicorn.run(
-        "app:app",
+        app,
         host="0.0.0.0",
-        port=5000,
-        reload=True,
+        port=port,
         log_level="info"
-    ) 
+    )
